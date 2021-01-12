5a0cbfe9b5907019ade6ee951fb14edbb1e08e3f
nsaedae@gmail.com <nsaedae@gmail.com>
2021-01-12 16:02:58 +0900

    2021/01/12 Naver 작업완료

---
+++ NaverWordCount/src/sub1/Mapper.java
@@ -0,0 +1,37 @@
+package sub1;
+
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+
+// Map 연산을 수행하는 클래스
+public class Mapper extends org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, IntWritable>{
+
+	private IntWritable one = new IntWritable(1);
+	private Text txt = new Text();
+	
+	@Override
+	protected void map(LongWritable key, Text value,
+			org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+			throws IOException, InterruptedException {
+		
+		String line = value.toString();
+		String[] tokens = line.split(",");
+		
+		txt.set(tokens[1]);
+		context.write(txt, one);
+	}
+}
+
+
+
+
+
+
+
+
